18241396aa027e796e81916fcf0f9026720d3a20
Nathan Mentley <NathanMentley@Gmail.com>
2015-11-05 17:48:20 -0800

    More folder structure changes. I'm just trying to get more organized before spec'ing out the next body of work.

---
+++ src/tools/JsBedRock.UnitTestRunner/src/Main.js
@@ -0,0 +1,14 @@
+(function (asm) {
+    asm.OnLoad(function () {
+        JsBedRock.Main = JsBedRock.Utils.ObjectOriented.CreateClass({
+            Constructor: function () {
+                JsBedRock.Utils.ObjectOriented.CallBaseConstructor(this, JsBedRock.Types.Object);
+            },
+            Members: {
+				Main: function () {
+                    
+                }
+            }
+        });
+    });
+})(JsBedRock.CurrentAssembly);
